67974bfe5ab775dcb48cd54215bf0b08300c01f5
Corey Morgan <wapitiasoft@gmail.com>
2017-12-01 08:28:35 -0700

    US0100 Remove old collateral source, ant build source

--- src/collateral/OLDCODE/CSVMoneyItemBuildException.java
+++
@@ -1,9 +0,0 @@
-package com.wapitia.common.csv;
-
-public class CSVMoneyItemBuildException extends RuntimeException {
-	private static final long serialVersionUID = 1L;
-
-	public CSVMoneyItemBuildException(String message) {
-		super(message);
-	}
-}
